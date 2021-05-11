dc4c9b946d1959dde4ca943e8e2a5cd07dd0bcb5
Dan Isaza <dan@isaza.dev>
2021-05-10 21:58:45 -0500

    WIP

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-"use strict";
-
-console.log("this is a message from the AMD module, version 1.0.3");
-while (typeof React === "undefined" || React === null) {
-  console.log("React is not available. Waiting 100ms and trying again.");
-  await sleep(100);
-}
-const garblegarble = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
+(async function () {
+  console.log("this is a message from the AMD module, version 1.0.3");
+  while (typeof React === "undefined" || React === null) {
+    console.log("React is not available. Waiting 100ms and trying again.");
+    await sleep(100);
   }
+  const garblegarble = React.createElement;
 
-  render() {
-    if (this.state.liked) {
-      return "You liked this.";
+  class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
     }
 
-    return garblegarble(
-      "button",
-      { onClick: () => this.setState({ liked: true }) },
-      "Like"
-    );
-  }
-}
+    render() {
+      if (this.state.liked) {
+        return "You liked this.";
+      }
 
-const domContainer = document.querySelector("#like_button_container");
-ReactDOM.render(garblegarble(LikeButton), domContainer);
+      return garblegarble(
+        "button",
+        { onClick: () => this.setState({ liked: true }) },
+        "Like"
+      );
+    }
+  }
+
+  const domContainer = document.querySelector("#like_button_container");
+  ReactDOM.render(garblegarble(LikeButton), domContainer);
+})();
